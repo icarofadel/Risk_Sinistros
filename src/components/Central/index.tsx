b30d3a572ace839dc0ddbb84d0a20d790281810c
RedSkull_7_7 <icaro.nata147@gmail.com>
2025-02-19 07:25:55 -0300

    Add Central (Incluindo campos de consultas e relatórios)

--- src/components/Central/index.tsx
+++ src/components/Central/index.tsx
@@ -4,17 +4,52 @@ import * as S from './styles'
 const Central = () => (
   <S.HomeSection>
     <h2>Sinistros</h2>
-    <S.ControlButtons>
-      <Botao type="link" to={`/CadSinistro`} title="Cadastro de sinistro">
-        Cadastrar sinistro
-      </Botao>
-      <Botao type="link" to={`/CadSinistro`} title="Cadastro de sinistro">
-        Cadastrar NC para parceiros
-      </Botao>
-      <Botao type="link" to={`/CadSinistro`} title="Cadastro de sinistro">
-        Cadastrar sinistro no seguro
-      </Botao>
-    </S.ControlButtons>
+    <S.ContainerButtons>
+      <S.Column>
+        <h3>Incluir registro</h3>
+        <S.ControlButtons>
+          <Botao type="link" to={`/CadSinistro`} title="Cadastro de sinistro">
+            Cadastrar sinistro
+          </Botao>
+          <Botao type="link" to={`/CadSinistro`} title="Cadastro de sinistro">
+            Cadastrar NC para parceiros
+          </Botao>
+          <Botao type="link" to={`/CadSinistro`} title="Cadastro de sinistro">
+            Cadastrar sinistro no seguro
+          </Botao>
+        </S.ControlButtons>
+      </S.Column>
+
+      <S.Column>
+        <h3>Consultar</h3>
+        <S.ControlButtons>
+          <Botao type="link" to={`/CadSinistro`} title="Consultar de sinistro">
+            Consultar sinistro
+          </Botao>
+          <Botao type="link" to={`/CadSinistro`} title="Consultar de sinistro">
+            Consultar NC para parceiros
+          </Botao>
+          <Botao type="link" to={`/CadSinistro`} title="Consultar de sinistro">
+            Consultar sinistro no seguro
+          </Botao>
+        </S.ControlButtons>
+      </S.Column>
+    </S.ContainerButtons>
+
+    <h3>Relatórios</h3>
+    <S.ContainerRelatorio>
+      <div>
+        <Botao type="link" to={`/CadSinistro`} title="Consultar de sinistro">
+          Consultar sinistro
+        </Botao>
+        <Botao type="link" to={`/CadSinistro`} title="Consultar de sinistro">
+          Consultar NC para parceiros
+        </Botao>
+        <Botao type="link" to={`/CadSinistro`} title="Consultar de sinistro">
+          Consultar sinistro no seguro
+        </Botao>
+      </div>
+    </S.ContainerRelatorio>
   </S.HomeSection>
 )
 
--- src/components/Central/styles.ts
+++ src/components/Central/styles.ts
@@ -2,7 +2,8 @@ import styled from 'styled-components'
 import { cores } from '../../styles'
 
 export const HomeSection = styled.section`
-  h2 {
+  h2,
+  h3 {
     color: ${cores.color};
     padding: 8px;
     display: flex;
@@ -12,6 +13,27 @@ export const HomeSection = styled.section`
 
 export const ControlButtons = styled.div`
   margin-top: 8px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+`
+
+export const ContainerButtons = styled.div`
   display: grid;
-  justify-items: start;
+  grid-template-columns: 1fr 1fr;
+  gap: 20px;
+`
+
+export const Column = styled.div`
+  text-align: center;
+`
+
+export const ContainerRelatorio = styled.div`
+  display: flex;
+  gap: 10px;
+
+  div {
+    display: flex;
+    align-items: center;
+  }
 `
