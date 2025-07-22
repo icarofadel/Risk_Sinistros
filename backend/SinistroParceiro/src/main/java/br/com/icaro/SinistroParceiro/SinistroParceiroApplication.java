ba3a34539b48633026b4de12465a2c5cc526c0e1
RedSkull_7_7 <icaro.nata147@gmail.com>
2025-07-22 07:55:08 -0300

    Add. microserviço SinistroParceiro

---
+++ backend/SinistroParceiro/src/main/java/br/com/icaro/SinistroParceiro/SinistroParceiroApplication.java
@@ -0,0 +1,13 @@
+package br.com.icaro.SinistroParceiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinistroParceiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SinistroParceiroApplication.class, args);
+	}
+
+}
---
+++ backend/SinistroParceiro/src/test/java/br/com/icaro/SinistroParceiro/SinistroParceiroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.icaro.SinistroParceiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinistroParceiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
