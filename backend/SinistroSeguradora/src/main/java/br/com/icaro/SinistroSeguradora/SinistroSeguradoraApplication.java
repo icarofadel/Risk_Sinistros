ba5e4ba76fd1e43634bf1dcd4e5d223e463750f9
RedSkull_7_7 <icaro.nata147@gmail.com>
2025-07-24 10:07:29 -0300

    Add: microserviço de cadastro de seguros

---
+++ backend/SinistroSeguradora/src/main/java/br/com/icaro/SinistroSeguradora/SinistroSeguradoraApplication.java
@@ -0,0 +1,13 @@
+package br.com.icaro.SinistroSeguradora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinistroSeguradoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SinistroSeguradoraApplication.class, args);
+	}
+
+}
---
+++ backend/SinistroSeguradora/src/test/java/br/com/icaro/SinistroSeguradora/SinistroSeguradoraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.icaro.SinistroSeguradora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinistroSeguradoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
