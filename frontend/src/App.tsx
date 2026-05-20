45f192206ac199c0542411b0529a7b29dea42f78
RedSkull_7_7 <icaro.nata147@gmail.com>
2026-05-20 20:58:05 -0300

    Fix: removendo reprodução automática de áudio e referência ao arquivo de música

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter } from 'react-router-dom'
-import { useEffect, useRef } from 'react'
 
 import { GlobalCss } from './styles'
 
@@ -8,21 +7,9 @@ import Footer from './components/Footer'
 import Rotas from './routes'
 
 function App() {
-  const audioRef = useRef<HTMLAudioElement>(null)
-
-  useEffect(() => {
-    if (audioRef.current) {
-      audioRef.current.play()
-    }
-  }, [])
-
   return (
     <>
       <GlobalCss />
-      <audio ref={audioRef} autoPlay loop>
-        <source src="/sons/music.mp3" type="audio/mp3" />
-        Seu navegador não suporta a tag de áudio.
-      </audio>
       <Header />
       <BrowserRouter>
         <div className="fundo">
